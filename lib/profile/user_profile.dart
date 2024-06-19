d1db418c24e46387014e8f38fcdd8005c5890635
PanMyintMo <panmyintmo257634@gmail.com>
2024-06-19 11:05:43 +0630

    conflict solve

---
+++ lib/profile/user_profile.dart
@@ -0,0 +1,349 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:youapp/enum/status.dart';
+import 'package:youapp/module/profile/profile_module.dart';
+import 'package:youapp/profile/bloc/profile_bloc.dart';
+import 'package:youapp/profile/response/profile_response.dart';
+import 'package:youapp/profile/user_profile_body.dart';
+import 'package:youapp/routes/profile/profile_routes.dart';
+import 'package:youapp/util/app_color.dart';
+import 'package:youapp/util/app_logger.dart';
+import 'package:youapp/util/app_router.dart';
+import 'package:youapp/util/app_string.dart';
+
+class UserProfile extends StatefulWidget {
+  const UserProfile({super.key});
+
+  @override
+  State<UserProfile> createState() => _UserProfileState();
+}
+
+class _UserProfileState extends State<UserProfile> {
+  bool show = false;
+  ProfileResponse? profileResponse;
+  File? image;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      BlocProvider.of<ProfileBloc>(context).add(GetUserProfileEvent());
+    });
+  }
+
+  void _handleUpdateProfile() {
+    setState(() {
+      show = !show;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProfileBloc, ProfileState>(
+      builder: (context, state) {
+        switch (state.status) {
+          case Status.loading:
+            const Center(child: CircularProgressIndicator());
+
+          case Status.failed:
+            const Center(child: Text('Failed'));
+
+          case Status.success:
+            profileResponse = state.response;
+            buildUserProfileHome();
+
+          default:
+        }
+        return buildUserProfileHome();
+      },
+    );
+  }
+
+  buildUserProfileHome() {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        actions: [
+          PopupMenuButton<int>(
+            itemBuilder: (BuildContext context) {
+              return [
+                const PopupMenuItem<int>(
+                  value: 0,
+                  child: Text(AppString.updateProfile),
+                ),
+              ];
+            },
+            icon: const FaIcon(
+              FontAwesomeIcons.ellipsis,
+              color: Colors.white,
+            ),
+            onSelected: (int value) {
+              _handleUpdateProfile();
+            },
+          ),
+        ],
+        elevation: 0,
+        title: Row(
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Image.asset(
+                "assets/images/back.png",
+                color: Colors.white,
+                width: 7,
+                height: 16,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 8.0),
+              child: Text(
+                "Back",
+                style: TextStyle(
+                  color: YouAppColor.whiteColor,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Text(
+                  profileResponse?.userData.username ?? '',
+                  style: const TextStyle(
+                    color: YouAppColor.whiteColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  color: YouAppColor.cardBackgroundColor,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 16,
+                      bottom: 45,
+                      child: Text(
+                        profileResponse?.userData.username ?? '',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 16,
+                      bottom: 20,
+                      child: Text(
+                        profileResponse?.userData.email ?? '',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              createProfile(),
+              const SizedBox(height: 20),
+              buildInterest(
+                  AppString.interest, AppString.profileInterestDescription),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildInterest(String title, String description) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: YouAppColor.cardBackgroundColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              IconButton(
+                onPressed: () {
+                  AppRouter.changeRoute<ProfileModule>(ProfileRoutes.interest);
+                },
+                icon: const Icon(Icons.edit, color: Colors.grey),
+              ),
+            ],
+          ),
+          const SizedBox(width: 10),
+          Text(
+            description,
+            style: const TextStyle(color: Colors.grey, fontSize: 14),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildInfoCard(String title, String description) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: YouAppColor.cardBackgroundColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              IconButton(
+                onPressed: () {
+                  if (title == "Interest") {
+                    AppRouter.changeRoute<ProfileModule>(
+                        ProfileRoutes.interest);
+                  } else {
+                    setState(() {
+                      show = !show;
+                    });
+                  }
+                },
+                icon: const Icon(Icons.edit, color: Colors.grey),
+              ),
+            ],
+          ),
+          const SizedBox(width: 10),
+          if (profileResponse?.userData.birthday != null &&
+              profileResponse?.userData.height != 0 &&
+              profileResponse?.userData.weight != 0) ...[
+            buildUserData(profileResponse!.userData)
+          ] else
+            Text(
+              description,
+              style: const TextStyle(color: Colors.grey, fontSize: 14),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildUserData(UserData userData) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        buildInfoRow('Birthday', userData.birthday),
+        buildInfoRow('Horoscope', userData.horoscope),
+        buildInfoRow('Height', '${userData.height} cm'),
+        buildInfoRow('Weight', '${userData.weight} kg'),
+      ],
+    );
+  }
+
+  Widget buildInfoRow(String label, String value) {
+    return value.isNotEmpty
+        ? Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: Row(
+              children: [
+                Text(
+                  '$label: ',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  value,
+                  style: const TextStyle(color: Colors.white, fontSize: 14),
+                ),
+              ],
+            ),
+          )
+        : Container();
+  }
+
+  getImage(File? image) {
+    setState(() {
+      logger.e("Image$image");
+      this.image = image;
+    });
+  }
+
+  Widget createProfile() {
+    return !show
+        ? buildInfoCard(AppString.about, AppString.profileAboutDescription)
+        : UserProfileBody(
+            callBackImage: getImage, profileResponse: profileResponse);
+  }
+
+  Widget buildTextField(String label, String placeholder) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style: const TextStyle(color: Colors.grey, fontSize: 14),
+          ),
+          const SizedBox(height: 5),
+          TextField(
+            decoration: InputDecoration(
+              hintText: placeholder,
+              hintStyle: const TextStyle(color: Colors.grey),
+              filled: true,
+              fillColor: Colors.grey[800],
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide.none,
+              ),
+            ),
+            style: const TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/update_interest.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:textfield_tags/textfield_tags.dart';
+import 'package:youapp/module/profile/profile_module.dart';
+
+import 'package:youapp/routes/profile/profile_routes.dart';
+import 'package:youapp/util/app_color.dart';
+import 'package:youapp/util/app_router.dart';
+import 'package:youapp/util/custom_app_bar.dart';
+import 'package:youapp/widgets/background.dart';
+
+class UpdateInterestWidget extends StatefulWidget {
+  const UpdateInterestWidget({super.key});
+
+  @override
+  State<UpdateInterestWidget> createState() => _UpdateInterestWidgetState();
+}
+
+class _UpdateInterestWidgetState extends State<UpdateInterestWidget> {
+  late double _distanceToField;
+  final StringTagController _stringTagController = StringTagController();
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _distanceToField = MediaQuery.of(context).size.width;
+  }
+
+  @override
+  void dispose() {
+    _stringTagController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: CustomAppBar(
+        appTitle: '',
+        onPressed: () {},
+        actions: [
+          TextButton(
+            onPressed: () {
+              AppRouter.changeRoute<ProfileModule>(ProfileRoutes.interest,
+                  interests: _stringTagController.getTags, isReplace: true);
+            },
+            child: const Text(
+              'Save',
+              style: TextStyle(color: YouAppColor.goldColor),
+            ),
+          ),
+        ],
+      ),
+      body: GradientBackground(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 4,
+              ),
+              const SizedBox(
+                child: Text(
+                  'Tell everyone about yourself',
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: YouAppColor.goldColor,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const SizedBox(
+                child: Text(
+                  'What interst you?',
+                  style: TextStyle(fontSize: 16, color: YouAppColor.whiteColor),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFieldTags<String>(
+                textfieldTagsController: _stringTagController,
+                initialTags: const [],
+                textSeparators: const [' ', ','],
+                letterCase: LetterCase.normal,
+                validator: (String tag) {
+                  if (tag == 'testing') {
+                    return 'testing not allowed';
+                  }
+                  return null;
+                },
+                inputFieldBuilder: (context, inputFieldValues) {
+                  return TextField(
+                    controller: inputFieldValues.textEditingController,
+                    focusNode: inputFieldValues.focusNode,
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    style: const TextStyle(color: YouAppColor.whiteColor),
+                    textInputAction: TextInputAction.done,
+                    cursorColor: YouAppColor.whiteColor,
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: YouAppColor.whiteColor,
+                          width: 3.0,
+                        ),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: YouAppColor.whiteColor,
+                          width: 3.0,
+                        ),
+                      ),
+                      helperText: 'Enter your interest...',
+                      helperStyle: const TextStyle(
+                        color: YouAppColor.whiteColor,
+                      ),
+                      hintText: inputFieldValues.tags.isNotEmpty
+                          ? ''
+                          : "Enter your interest...",
+                      errorText: inputFieldValues.error,
+                      hintStyle: const TextStyle(color: YouAppColor.whiteColor),
+                      prefixIconConstraints:
+                          BoxConstraints(maxWidth: _distanceToField * 0.74),
+                      prefixIcon: inputFieldValues.tags.isNotEmpty
+                          ? Wrap(
+                              direction: Axis.horizontal,
+                              spacing: 8.0,
+                              children:
+                                  inputFieldValues.tags.map((String tagData) {
+                                return Container(
+                                  decoration: const BoxDecoration(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(5.0),
+                                    ),
+                                    color: Color.fromRGBO(255, 255, 255, 0.1),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 10.0,
+                                    vertical: 4.0,
+                                  ),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      InkWell(
+                                        child: Text(
+                                          '#$tagData',
+                                          style: const TextStyle(
+                                            color: YouAppColor.whiteColor,
+                                          ),
+                                        ),
+                                        onTap: () {},
+                                      ),
+                                      const SizedBox(width: 4.0),
+                                      InkWell(
+                                        child: const Icon(
+                                          Icons.cancel,
+                                          size: 14.0,
+                                          color: Color.fromARGB(
+                                              255, 233, 233, 233),
+                                        ),
+                                        onTap: () {
+                                          inputFieldValues.onTagDelete(tagData);
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }).toList(),
+                            )
+                          : null,
+                    ),
+                    onSubmitted: (value) {
+                      if (_stringTagController.getValidator != null) {
+                        _stringTagController.setError =
+                            _stringTagController.getValidator!(value);
+                        if (_stringTagController.getError == null) {
+                          setState(() {
+                            _stringTagController.addTag(value);
+                          });
+                        }
+                      } else {
+                        setState(() {
+                          _stringTagController.addTag(value);
+                        });
+                      }
+                      inputFieldValues.textEditingController.clear();
+                      inputFieldValues.focusNode.requestFocus();
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class StringTagController extends TextfieldTagsController<String> {
+  @override
+  bool? addTag(String tag) {
+    bool? isAdded = super.addTag(tag);
+    notifyListeners();
+    return isAdded;
+  }
+
+  @override
+  set setError(String? error) {
+    super.setError = error;
+    getTextEditingController?.clear();
+    getFocusNode?.requestFocus();
+    notifyListeners();
+  }
+}
